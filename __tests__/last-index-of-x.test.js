0753151d0a5fbf8fa4a837ff2402815137b7eae5
Graham Fairweather <xotic750@gmail.com>
2019-07-17 00:22:06 +0200

    Terraformed
    Auto build

--- __tests__/last-index-of-x.test.js
+++ __tests__/last-index-of-x.test.js
@@ -1,29 +1,5 @@
 let lastIndexOf;
 
-if (typeof module === 'object' && module.exports) {
-  require('es5-shim');
-  require('es5-shim/es5-sham');
-
-  if (typeof JSON === 'undefined') {
-    JSON = {};
-  }
-
-  require('json3').runInContext(null, JSON);
-  require('es6-shim');
-  const es7 = require('es7-shim');
-  Object.keys(es7).forEach(function(key) {
-    const obj = es7[key];
-
-    if (typeof obj.shim === 'function') {
-      obj.shim();
-    }
-  });
-
-  lastIndexOf = require('../../index.js');
-} else {
-  lastIndexOf = returnExports;
-}
-
 const itHasDoc = typeof document !== 'undefined' && document ? it : xit;
 
 // IE 6 - 8 have a bug where this returns false.
