c17d15bc66d04c5b2790bb0897803028251a55ef
Graham Fairweather <xotic750@gmail.com>
2019-07-22 01:45:37 +0200

    :bookmark: v3.0.3
    Auto build

--- dist/last-index-of-x.js
+++ dist/last-index-of-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-19T23:14:28.600Z",
+  "date": "2019-07-21T23:45:31.452Z",
   "describe": "",
   "description": "An extended ES6 lastIndexOf.",
   "file": "last-index-of-x.js",
   "hash": "804aed6f4b632a297664",
   "license": "MIT",
-  "version": "3.0.2"
+  "version": "3.0.3"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
